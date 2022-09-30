832d1de4e9adf6b3c96e88c996ea507b941bb4b5
charlesponti <cj@ponti.io>
2022-09-29 20:47:19 -0700

    feat(AlertError) Add AlertError component

--- src/scenes/Account/AccountEdit.tsx
+++ src/scenes/Account/AccountEdit.tsx
@@ -1,10 +1,11 @@
 import { useCallback, useState } from 'react';
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
+import AlertError from 'components/Alerts/Error';
 import { setUser } from 'services/auth';
 import { ACCOUNT_PATH } from 'services/constants/routes';
 import { AppState, authSelectors, store } from 'services/store';
-
 import { useAccountEdit } from 'services/supabase';
 
 function AccountEdit({ user }: { user: User }) {
@@ -26,14 +27,7 @@ function AccountEdit({ user }: { user: User }) {
       <h1 className="text-4xl mb-8 mt-8 font-bold">
         Edit profile
       </h1>
-      {error ? (
-        <div className="alert alert-error shadow-lg mb-8">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <span>There was an issue updating your profile.</span>
-          </div>
-        </div>
-      ) : null}
+      {error ? <AlertError message="There was an issue updating your profile." /> : null}
       {loading ? (
         <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed" disabled={loading}>
           <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -53,6 +47,7 @@ function AccountEdit({ user }: { user: User }) {
                 className="input input-md input-bordered w-full max-w-xs"
                 value={name || ''}
                 onChange={onSetUsername}
+                disabled={loading}
               />
             </label>
           </div>
--- src/scenes/Auth/index.tsx
+++ src/scenes/Auth/index.tsx
@@ -1,3 +1,4 @@
+import AlertError from 'components/Alerts/Error';
 import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useLogin } from 'services/supabase';
@@ -20,18 +21,9 @@ function Auth({ user }: AuthProps) {
   return (
     <div className="row flex-center flex justify-items-center w-full mt-16">
       <div className="col-6 form-widget mx-auto" aria-live="polite">
-        <h1 className="text-4xl mb-4 font-bold">
-          Sign In
-        </h1>
+        <h1 className="text-4xl mb-4 font-bold">Sign In</h1>
 
-        {error ? (
-          <div className="alert alert-error shadow-lg">
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" className="text-black stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-              <span className="text-black">{error.message}</span>
-            </div>
-          </div>
-        ) : null}
+        {error ? <AlertError message={error.message} /> : null}
 
         {loading ? <div>Sending magic link...</div> : null}
 
