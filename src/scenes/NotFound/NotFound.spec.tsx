9977ad334fbbc36108a6bbb9266efe44a98a7537
charlesponti <cj@ponti.io>
2022-10-09 19:11:23 -0700

    feat(NotFound) Update design of NotFound scene

---
+++ src/scenes/NotFound/NotFound.spec.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+
+import NotFound from "./NotFound";
+
+test("NotFound", () => {
+  render(<NotFound />);
+  expect(screen.getByTestId("not-found-img")).toBeVisible();
+});
--- src/scenes/NotFound/index.tsx
+++ src/scenes/NotFound/index.tsx
@@ -1,5 +1,15 @@
-function NotFound() {
-  return <div>Not Found</div>;
+import { lazy, Suspense } from "react";
+
+import Loading from "scenes/Loading";
+
+const NotFound = lazy(() => import("./NotFound"));
+
+function LazyNotFound() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <NotFound />
+    </Suspense>
+  );
 }
 
-export default NotFound;
+export default LazyNotFound;
