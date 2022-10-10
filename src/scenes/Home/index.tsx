67c07796fc88ba6fc8801125f235ac4d1383a21d
charlesponti <cj@ponti.io>
2022-10-09 22:13:00 -0700

    feat(*) Add GetStartedButton

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,3 +1,5 @@
+import GetStartedButton from "components/Buttons/GetStarted";
+
 function Home() {
   return (
     <div className="relative">
@@ -12,28 +14,16 @@ function Home() {
         <div className="relative pt-36 ml-auto">
           <div className="lg:w-2/3 text-center mx-auto">
             <h1 className="text-gray-400 dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">
-              Shaping a world with{" "}
-              <span className="text-primary dark:text-white">
-                reimagination.
-              </span>
+              A UI boilerplate for{" "}
+              <span className="text-primary dark:text-white">imagination.</span>
             </h1>
-            <p className="mt-8 text-gray-300 dark:text-gray-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
-              incidunt nam itaque sed eius modi error totam sit illum. Voluptas
-              doloribus asperiores quaerat aperiam. Quidem harum omnis beatae
-              ipsum soluta!
-            </p>
+            <p className="mt-8 text-gray-300 dark:text-gray-300" />
             <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+              <GetStartedButton />
               <a
-                href="/signin"
-                className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
-              >
-                <span className="relative text-base font-semibold text-gray-500">
-                  Get started
-                </span>
-              </a>
-              <a
-                href="/learn"
+                rel="noreferrer"
+                target="_blank"
+                href="https://github.com/theponti/ponti-ui"
                 className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
               >
                 <span className="relative text-base font-semibold text-primary dark:text-white">
