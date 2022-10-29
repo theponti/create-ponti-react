a576250c5ea8a11a2009157fa5bc3c9c3306f2b3
charlesponti <cj@ponti.io>
2022-10-29 14:36:34 -0700

    chore(*): upgrade dependencies

--- src/components/PageWrap/index.tsx
+++ src/components/PageWrap/index.tsx
@@ -5,7 +5,7 @@ type PageWrapProps = {
 };
 function PageWrap({ children }: PageWrapProps) {
   return (
-    <div className="row flex-center flex justify-items-center w-full mt-16 sm:px-4">
+    <div className="row flex-center flex justify-items-center w-full mt-16 px-4">
       <div className="w-full xl:w-4/5 md:mx-auto">{children}</div>
     </div>
   );
--- src/scenes/Dashboard/Dashboard.tsx
+++ src/scenes/Dashboard/Dashboard.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { supabase } from "services/supabase";
+
 import PageWrap from "components/PageWrap";
 
 function Dashboard() {
+  useEffect(() => {
+    const fetchData = async () => {
+      await supabase.from("ideas").select();
+    };
+    fetchData();
+  }, []);
   return (
     <PageWrap>
       <h3 className="text-3xl">Dashboard</h3>
